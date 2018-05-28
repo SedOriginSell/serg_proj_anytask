ad67e21dbcb9325f4592ae003a86d6a6345c9291
SedOriginSell <SedOriginSell@yandex.ru>
2018-05-29 00:15:40 +0500

    web

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	public class HomeController : Controller
+	{
+		private ApplicationDbContext db = new ApplicationDbContext();
+
+		// GET: DbMegafons
+		public ActionResult Index()
+		{
+			return View(db.User.ToList());
+		}
+
+		// GET: DbMegafons/Details/5
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			DbMegafon dbMegafon = db.User.Find(id);
+			if (dbMegafon == null)
+			{
+				return HttpNotFound();
+			}
+			return View(dbMegafon);
+		}
+
+		// GET: DbMegafons/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: DbMegafons/Create
+		// Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+		// сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include = "ID,PhoneNumber,FullName,Address,BuyDate,Tariff")] DbMegafon dbMegafon)
+		{
+			if (ModelState.IsValid)
+			{
+				db.User.Add(dbMegafon);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View(dbMegafon);
+		}
+
+		// GET: DbMegafons/Edit/5
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			DbMegafon dbMegafon = db.User.Find(id);
+			if (dbMegafon == null)
+			{
+				return HttpNotFound();
+			}
+			return View(dbMegafon);
+		}
+
+		// POST: DbMegafons/Edit/5
+		// Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+		// сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "ID,PhoneNumber,FullName,Address,BuyDate,Tariff")] DbMegafon dbMegafon)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(dbMegafon).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(dbMegafon);
+		}
+
+		// GET: DbMegafons/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			DbMegafon dbMegafon = db.User.Find(id);
+			if (dbMegafon == null)
+			{
+				return HttpNotFound();
+			}
+			return View(dbMegafon);
+		}
+
+		// POST: DbMegafons/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			DbMegafon dbMegafon = db.User.Find(id);
+			db.User.Remove(dbMegafon);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
---
+++ WebApplication1/Models/IdentityModels.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+using megafon_base;
+
+
+namespace WebApplication1.Models
+{
+    // В профиль пользователя можно добавить дополнительные данные, если указать больше свойств для класса ApplicationUser. Подробности см. на странице https://go.microsoft.com/fwlink/?LinkID=317594.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Обратите внимание, что authenticationType должен совпадать с типом, определенным в CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Здесь добавьте утверждения пользователя
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+			Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ApplicationDbContext>());
+		}
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+		public DbSet<DbMegafon> User { get; set; }
+	}
+
+
+	public class DbMegafon
+	{
+		[Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int ID { get; set; }
+		//номер телефона
+		public string PhoneNumber { get; set; }
+
+		//ФИО владельца
+		public string FullName { get; set; }
+
+		//адрес владельца
+		public string Address { get; set; }
+
+		//дата приобретения сим карты
+		public DateTime BuyDate { get; set; }
+
+		//тариф
+		public Rate Tariff { get; set; }
+
+
+		public DbMegafon()
+		{
+			FullName = "";
+			PhoneNumber = "";
+			Address = "";
+			BuyDate = DateTime.Now;
+			Tariff = Rate.ПЕРВЫЙ;
+		}
+
+		public static Rate GetRate(int indx)
+		{
+			switch (indx)
+			{
+				case 0:
+					return Rate.ПЕРВЫЙ;
+				case 1:
+					return Rate.ВТОРОЙ;
+				case 2:
+					return Rate.ТРЕТИЙ;
+			}
+			return Rate.ПЕРВЫЙ;
+		}
+		public static int GetRateIndx(Rate a)
+		{
+			switch (a)
+			{
+				case Rate.ПЕРВЫЙ:
+					return 0;
+				case Rate.ВТОРОЙ:
+					return 1;
+				case Rate.ТРЕТИЙ:
+					return 2;
+			}
+			return 0;
+		}
+	}
+
+	public enum Rate
+	{
+		ПЕРВЫЙ = 560,
+		ВТОРОЙ = 890,
+		ТРЕТИЙ = 1299,
+	}
+}
